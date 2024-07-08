6c038bf1fcd08bd890deff9a59ea6714fc40b0d7
Carolina Teixeira <maria.silva@creathus.org.br>
2024-07-07 22:30:10 -0400

    :sparkles: feat: add post product method

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.crud.controllers;
 
+import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -16,5 +18,13 @@ public class ProductController {
     public ResponseEntity getAllProducts(){
         var allProducts = repository.findAll();
         return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        Product newProduct = new Product(data);
+//        System.out.println("Produto registrado: " + data);
+        repository.save(newProduct);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
